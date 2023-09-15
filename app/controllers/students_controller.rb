bda6209ac3e0b49a2b7d20757d504868e6eb2e1d
Kennedy Musau <kennedy.musauu@gmail.com>
2023-09-16 01:40:39 +0300

    update student controller

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,16 +1,48 @@
 class StudentsController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :render_record_not_found_response
+  rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+
   def index
-    render json: Student.all
+    render json: Student.all, status: :ok
   end
 
   def show
-    student = Student.find_by(id: params[:id])
-    render json: student
+    student = find_student
+    render json: student, status: :ok
   end
 
   def create
+    student = Student.create!(student_params)
+    render json: student, status: :created
+  end
+
+  def update
+    student = find_student
+    student.update!(student_params)
+    render json: student, status: :created
   end
 
   def destroy
+    student = find_student
+    student.destroy
+    head :no_content
+  end
+
+  private
+
+  def find_student
+    Student.find(params[:id])
+  end
+
+  def student_params
+    params.permit(:name, :major, :age, :instructor_id)
+  end
+
+  def render_record_not_found_response
+    render json: { error: "Student not found" }, status: :not_found
+  end
+
+  def render_unprocessable_entity_response(invalid)
+    render json: { errors: invalid.record.errors.full_messages }, status: :unprocessable_entity
   end
 end
