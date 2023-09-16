9ad3d8eb79e8bcfecce346bdafe3fe24e3c8ef9c
Kennedy Musau <kennedy.musauu@gmail.com>
2023-09-16 14:33:34 +0300

    update instructor

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,6 +1,5 @@
 class StudentsController < ApplicationController
   rescue_from ActiveRecord::RecordNotFound, with: :render_record_not_found_response
-  rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
 
   def index
     render json: Student.all, status: :ok
@@ -40,9 +39,5 @@ class StudentsController < ApplicationController
 
   def render_record_not_found_response
     render json: { error: "Student not found" }, status: :not_found
-  end
-
-  def render_unprocessable_entity_response(invalid)
-    render json: { errors: invalid.record.errors.full_messages }, status: :unprocessable_entity
   end
 end
